1f217626ff29295363cfbb02f78f23ffaccbaeeb
AkanN17 <xaa8tq@virginia.edu>
2023-07-14 15:48:51 -0400

    different error thrown now

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
+use std::error::Error;
+
 use paho_mqtt as mqtt;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Creating new client...");
-    let client = mqtt::AsyncClient::new("ssl://nam1.cloud.thethings.network:8883").unwrap();
+    let client = mqtt::AsyncClient::new("tcp://nam1.cloud.thethings.network:8883")?;
 
     let conn_opts = mqtt::ConnectOptionsBuilder::new()
         .server_uris(&["ssl://nam1.cloud.thethings.network:8883"])
         .keep_alive_interval(std::time::Duration::from_secs(30))
         .clean_session(true)
         .user_name("uva-engineers-way-sensors@ttn")
-        .password("ZXDUEUJ7XISNZNAE4RR3ZMNJX4XXL66LS5VJXOA")
+        .password("NNSXS.H74NN25HX2EFPPAH5JMMZXJBDRQP3KIBMOIF3RI.FLFWZZDQPJVUA4Z2FKZG6RRQNJC37I2B5BTZ464YY5P5OG2M47EQ")
         .finalize();
 
     println!("Connecting to the MQTT broker...");
-    let res = client.connect(conn_opts).wait();
-
-    match res {
-        Ok(_) => println!("Successfully connected to the broker!"),
-        Err(e) => {
-            println!("Failed to connect to the broker. {:?}", e);
-            return;
-        }
-    }
+    client.connect(conn_opts).wait()?;
 
     let subscribe_topic = "v3/uva-engineers-way-sensors@ttn/devices/+/up";
     println!("Subscribing to the topic: {}", subscribe_topic);
-    client.subscribe(subscribe_topic, 1).wait().unwrap();
+    client.subscribe(subscribe_topic, 0).wait()?;
 
     let stream = client.start_consuming();
     while let Ok(Some(message)) = stream.try_recv() {
@@ -35,4 +29,5 @@ fn main() {
     }
 
     let _ = client.disconnect(None);
+    Ok(())
 }

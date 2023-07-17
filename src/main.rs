9dcc2e7abdcd36cee7421dff319c5884a4db7dd7
AkanN17 <xaa8tq@virginia.edu>
2023-07-17 12:09:52 -0400

    Saving what I have

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
-use std::error::Error;
-
 use paho_mqtt as mqtt;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> mqtt::Result<()> {
     println!("Creating new client...");
-    let client = mqtt::AsyncClient::new("tcp://nam1.cloud.thethings.network:8883")?;
+
+    let server_uri = "ssl://nam1.cloud.thethings.network:8883";
+    let username = "uva-engineers-way-sensors@ttn";
+    let password = "NNSXS.H74NN25HX2EFPPAH5JMMZXJBDRQP3KIBMOIF3RI.FLFWZZDQPJVUA4Z2FKZG6RRQNJC37I2B5BTZ464YY5P5OG2M47EQ";
+    let subscribe_topic = "v3/uva-engineers-way-sensors@ttn/devices/+/up";
+
+    println!("Server URI: {}", server_uri);
+    println!("Username: {}", username);
+    println!("Password: {}", password);
+    println!("Subscribe Topic: {}", subscribe_topic);
+
+    let client = mqtt::AsyncClient::new(server_uri)?;
 
     let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .server_uris(&["ssl://nam1.cloud.thethings.network:8883"])
+        .server_uris(&[server_uri])
         .keep_alive_interval(std::time::Duration::from_secs(30))
         .clean_session(true)
-        .user_name("uva-engineers-way-sensors@ttn")
-        .password("NNSXS.H74NN25HX2EFPPAH5JMMZXJBDRQP3KIBMOIF3RI.FLFWZZDQPJVUA4Z2FKZG6RRQNJC37I2B5BTZ464YY5P5OG2M47EQ")
+        .user_name(username)
+        .password(password)
         .finalize();
 
     println!("Connecting to the MQTT broker...");
-    client.connect(conn_opts).wait()?;
+    client.connect(conn_opts)?;
 
-    let subscribe_topic = "v3/uva-engineers-way-sensors@ttn/devices/+/up";
+    println!("Successfully connected to the broker!");
     println!("Subscribing to the topic: {}", subscribe_topic);
-    client.subscribe(subscribe_topic, 0).wait()?;
+
+    client.subscribe(subscribe_topic, 0)?;
 
     let stream = client.start_consuming();
     while let Ok(Some(message)) = stream.try_recv() {
-        // Handle the received message
         println!("Received message: {:?}", message.payload_str());
-        // Process the message payload and extract the relevant data
     }
 
-    let _ = client.disconnect(None);
-    Ok(())
+    Ok(client.disconnect(None)?)
 }

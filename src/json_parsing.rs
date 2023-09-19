8f56010a48a2b2a1d62233786bf70f1d250a4b44
AkanN17 <xaa8tq@virginia.edu>
2023-09-19 14:06:41 -0400

    added some parsing for metadata

--- src/json_parsing.rs
+++ src/json_parsing.rs
@@ -2,7 +2,6 @@ use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use serde_json::Value;
 
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TTNPayload {
     #[serde(rename = "end_device_ids")]
@@ -31,6 +30,7 @@ struct UplinkMessage {
 struct RxMetadata {
     #[serde(rename = "gateway_ids")]
     gateway_ids: GatewayIDs,
+    rssi: i32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -41,6 +41,25 @@ struct GatewayIDs {
 
 pub fn parse_payload(json_str: &str) -> Result<TTNPayload, serde_json::Error> {
     serde_json::from_str(json_str)
+}
+
+pub fn extract_metadata(payload: &TTNPayload) -> HashMap<String, Value> {
+    let mut metadata = HashMap::new();
+
+    // // Find the best RSSI gateway
+    // let best_rssi_metadata = payload.uplink_message.rx_metadata.iter()
+    //     .max_by_key(|meta| meta.rssi);
+
+    if let Some(best_rssi_meta) = best_rssi_metadata {
+        metadata.insert("rssi".to_string(), Value::Number(best_rssi_meta.rssi.into()));
+        metadata.insert("gateway_id".to_string(), Value::String(best_rssi_meta.gateway_ids.gateway_id.clone()));
+    }
+
+    metadata.insert("received_time".to_string(), Value::String(payload.received_at.clone()));
+    metadata.insert("device_id".to_string(), Value::String(payload.end_device_ids.dev_eui.clone()));
+    metadata.insert("receiver".to_string(), Value::String("http-ttn-mqtt".to_string()));
+
+    metadata
 }
 
 // Function that helps extract dynamic fields from the uplink message

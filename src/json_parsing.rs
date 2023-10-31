a6d075717d107e11f3ee99fc4cd6c077a4c74601
AkanN17 <xaa8tq@virginia.edu>
2023-10-31 14:12:42 -0400

    Cleaning the parsing file

--- src/json_parsing.rs
+++ src/json_parsing.rs
@@ -100,34 +100,34 @@ pub fn parse_payload(json_str: &str) -> Result<TTNPayload, serde_json::Error> {
 
 // This function parses the metadata and stores it in a HashMap with a string for the field's
 // name and its value. Is currently not used
-pub fn extract_metadata(payload: &TTNPayload) -> HashMap<String, Value> {
-    let mut metadata = HashMap::new();
+// pub fn extract_metadata(payload: &TTNPayload) -> HashMap<String, Value> {
+//     let mut metadata = HashMap::new();
 
-    // Finding the best RSSI gateway
-    let best_rssi_metadata = payload.uplink_message.rx_metadata.iter()
-        .max_by_key(|meta| meta.rssi);
-
-    if let Some(best_rssi_meta) = best_rssi_metadata {
-        metadata.insert("rssi".to_string(), Value::Number(best_rssi_meta.rssi.into()));
-        metadata.insert("gateway_id".to_string(), Value::String(best_rssi_meta.gateway_ids.gateway_id.clone()));
-    }
-
-    metadata.insert("received_time".to_string(), Value::String(payload.uplink_message.received_at.clone()));
-    metadata.insert("device_id".to_string(), Value::String(payload.end_device_ids.dev_eui.clone()));
-    metadata.insert("receiver".to_string(), Value::String("http-ttn-mqtt".to_string()));
-
-    metadata
-}
-
-// Function that helps extract dynamic fields from the uplink message; Currently is not used
-pub fn extract_numeric_fields(payload: &TTNPayload) -> HashMap<String, f64> {
-    let mut numeric_fields = HashMap::new();
-    for (key, value) in payload.uplink_message.decoded_payload.iter() {
-        if let Value::Number(num) = value {
-            if let Some(n) = num.as_f64() {
-                numeric_fields.insert(key.clone(), n);
-            }
-        }
-    }
-    numeric_fields
-}
+//     // Finding the best RSSI gateway
+//     let best_rssi_metadata = payload.uplink_message.rx_metadata.iter()
+//         .max_by_key(|meta| meta.rssi);
+//
+//     if let Some(best_rssi_meta) = best_rssi_metadata {
+//         metadata.insert("rssi".to_string(), Value::Number(best_rssi_meta.rssi.into()));
+//         metadata.insert("gateway_id".to_string(), Value::String(best_rssi_meta.gateway_ids.gateway_id.clone()));
+//     }
+//
+//     metadata.insert("received_time".to_string(), Value::String(payload.uplink_message.received_at.clone()));
+//     metadata.insert("device_id".to_string(), Value::String(payload.end_device_ids.dev_eui.clone()));
+//     metadata.insert("receiver".to_string(), Value::String("http-ttn-mqtt".to_string()));
+//
+//     metadata
+// }
+//
+// // Function that helps extract dynamic fields from the uplink message; Currently is not used
+// pub fn extract_numeric_fields(payload: &TTNPayload) -> HashMap<String, f64> {
+//     let mut numeric_fields = HashMap::new();
+//     for (key, value) in payload.uplink_message.decoded_payload.iter() {
+//         if let Value::Number(num) = value {
+//             if let Some(n) = num.as_f64() {
+//                 numeric_fields.insert(key.clone(), n);
+//             }
+//         }
+//     }
+//     numeric_fields
+// }
